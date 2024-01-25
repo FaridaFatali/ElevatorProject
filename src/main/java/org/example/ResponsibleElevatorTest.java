5485cd21421e3a377c9e1b24159201247f36c6fd
Farida Fatali <faridafatali.91@gmail.com>
2024-01-25 15:44:33 +0400

    Update ResponsibleElevatorTest.java

--- src/main/java/org/example/ResponsibleElevatorTest.java
+++ src/main/java/org/example/ResponsibleElevatorTest.java
@@ -6,7 +6,7 @@ public class ResponsibleElevatorTest {
         elevator.openDoor();  // Get into the elevator
         elevator.go(Floor.FIFTH);
         System.out.println();
-        elevator.openDoor();  // If somebody is comes
+        elevator.openDoor();  // If somebody comes
         elevator.go(Floor.FIRST);
         System.out.println();
         elevator.go(Floor.FIFTH);
